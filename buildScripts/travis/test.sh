5d9c9fd9c04bbd19977288ea631bacb23be98e5a
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-01 10:22:50 +0300

    Move download to the EXECUTOR

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-curl --output tests_qrs.zip "https://dl.trikset.com/edu/.solutions20200701/test.zip"
-$EXECUTOR bash -lixc "7z x tests_qrs.zip"
+$EXECUTOR bash -lixc "curl --output tests_qrs.zip https://dl.trikset.com/edu/.solutions20200701/test.zip && 7z x tests_qrs.zip"
 if [ $TRAVIS_OS_NAME = "linux" ]
 then
 	$EXECUTOR bin/$CONFIG/build-checker-installer.sh
