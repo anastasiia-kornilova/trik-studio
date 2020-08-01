4c43232a3be5efaee26fc0433e3dbb63ca99583a
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-01 18:16:26 +0300

    More restrictions on lang

--- buildScripts/travis/test.sh
+++ buildScripts/travis/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-export LANG=en_US.UTF-8 && $EXECUTOR bash -lixc "curl --output tests_qrs.zip https://dl.trikset.com/edu/.solutions20200701/test.zip && 7z x tests_qrs.zip && ls testing"
+export LANG=en_US.UTF-8 && $EXECUTOR bash -lixc "curl --output tests_qrs.zip https://dl.trikset.com/edu/.solutions20200701/test.7z && 7z x tests_qrs.zip && ls testing"
 if [ $TRAVIS_OS_NAME = "linux" ]
 then
 	$EXECUTOR bin/$CONFIG/build-checker-installer.sh
