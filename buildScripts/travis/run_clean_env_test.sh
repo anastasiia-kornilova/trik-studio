22999337dfb64d030c631f42361bc72e934339e7
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 19:48:40 +0300

    Add platform minimal

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -6,6 +6,4 @@ curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z
 
 curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
 chmod +x installer.run
-ldd ./installer.run
-./installer.run
-ls -la .
+./installer.run -platform minimal --script ./installer/trik_studio_installscript.qs
