be4c5b87163687e8e4750fb559909132e06815ee
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 18:19:52 +0300

    Add installation step

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-curl --output installer.sh "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
+
+curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
+chmod +x installer.run
+./installer.run --script ./installer/trik_studio_installscript.qs
 
