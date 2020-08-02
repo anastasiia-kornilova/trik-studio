a73e2b957904bfd5f19e6561d8981f94fd1c26b8
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 20:12:10 +0300

    Look up

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -7,5 +7,4 @@ curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z
 curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
 chmod +x installer.run
 ./installer.run -v --platform minimal --script ./installer/trik_studio_installscript.qs
-ls -la
-./buildScripts/tests/fieldstest.py ./TRIKStudio/bin/2D-model testing
+./buildScripts/tests/fieldstest.py /home/travis/TRIKStudio/bin/2D-model testing
