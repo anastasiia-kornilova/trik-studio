5578ecd81c8d423f0e757db4bbec0a5b0b1b2006
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-02 18:50:23 +0300

    Debugging

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -7,4 +7,5 @@ curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z
 curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
 chmod +x installer.run
 ldd ./installer.run
+./installer.run
 ls -la .
