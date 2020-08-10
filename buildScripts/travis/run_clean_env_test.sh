f15829b2c7fbae5fd08994b6fe0e35af6814443e
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-10 12:06:09 +0300

    Fix installer path

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -18,7 +18,7 @@ esac
 curl --output installer.dmg "${INSTALLER_URL}"
 sudo hdiutil attach installer.dmg
 ls -la /Volumes/
-sudo /Volumes/trik-studio-offline-mac-installer-7a6802 --platform minimal --script ./installer/trik_studio_installscript.qs
+sudo /Volumes/trik-studio-offline-mac-installer-7a6802 --platform minimal
 sudo installer -package /Volumes/installer/installer.pkg -target /
 ./installer.dmg --platform minimal --script ./installer/trik_studio_installscript.qs
 env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
