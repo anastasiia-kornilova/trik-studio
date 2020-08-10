c8b6d487d3d71e06860fdc252f1cfbf2cb978d7c
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-10 10:15:29 +0300

    Differ linux and mac machines

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -4,7 +4,18 @@ set -euxo pipefail
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
 
-curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-{$MACHINE_NAME}-master.run"
+INSTALLER_POSTFIX=""
+case $TRAVIS_OS_NAME in
+  osx)
+    INSTALLER_POSTFIX="mac"
+    ;;
+  linux)
+    INSTALLER_POSTFIX="linux"
+    ;;
+  *) exit 1 ;;
+esac
+
+curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-${INSTALLER_POSTFIX}-master.run"
 chmod +x installer.run
 ./installer.run --platform minimal --script ./installer/trik_studio_installscript.qs
 env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
