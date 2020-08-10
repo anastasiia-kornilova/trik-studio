174b6c8dbe0ed8099f80a6ec0ab977aeb5383d98
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-10 17:32:46 +0300

    Add minimal env plugin

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -4,6 +4,5 @@ set -euxo pipefail
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
 
-./installer/install_studio_mac.sh ./installer/
-curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-mac-master.dmg"
+env QT_QPA_PLATFORM=minimal ./installer/install_studio_mac.sh ./installer/
 env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
