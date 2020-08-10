89f72ff8713718eb45a11719c3b53dd1c4b7ce75
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-10 10:30:05 +0300

    Fix installer path

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -4,18 +4,18 @@ set -euxo pipefail
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
 
-INSTALLER_POSTFIX=""
+INSTALLER_URL=""
 case $TRAVIS_OS_NAME in
   osx)
-    INSTALLER_POSTFIX="mac"
+    INSTALLER_URL="https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-mac-master.dmg"
     ;;
   linux)
-    INSTALLER_POSTFIX="linux"
+    INSTALLER_URL="https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-linux-master.run"
     ;;
   *) exit 1 ;;
 esac
 
-curl --output installer.run "https://dl.trikset.com/ts/fresh/installer/trik-studio-installer-${INSTALLER_POSTFIX}-master.run"
+curl --output installer.run "${INSTALLER_URL}"
 chmod +x installer.run
 ./installer.run --platform minimal --script ./installer/trik_studio_installscript.qs
 env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
