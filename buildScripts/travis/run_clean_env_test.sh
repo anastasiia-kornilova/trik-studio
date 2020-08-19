b89a9fe33692323dcfdf5f95d4f581609ddec2e2
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2020-08-19 20:24:01 +0300

    Offscreen plugin

--- buildScripts/travis/run_clean_env_test.sh
+++ buildScripts/travis/run_clean_env_test.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 curl --output test_qrs.7z "https://dl.trikset.com/edu/.solutions20200701/test.7z"
 7z x test_qrs.7z
 
-./installer/install_studio_mac.sh ./installer/
+env QT_QPA_PLATFORM=offscreen ./installer/install_studio_mac.sh ./installer/
 env QT_QPA_PLATFORM=minimal python3.7 ./buildScripts/tests/fieldstest.py "$HOME/TRIKStudio/bin/2D-model" testing
